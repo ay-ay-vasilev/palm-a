edd7ed06fcdcadf7eb55267ea3930e921929d1bb
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-10-25 00:58:55 +0900

    Made a character that moves horizontally with basic animations. Bugged (automatically goes to left idle state)

--- Classes/LevelScene.cpp
+++ Classes/LevelScene.cpp
@@ -20,23 +20,74 @@ bool Level::init()
     {
         return false;
     }
+    auto director = cocos2d::Director::getInstance();
+    
+    auto visibleSize = director->getVisibleSize();
+    Vec2 origin = director->getVisibleOrigin();
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    director->setProjection(Director::Projection::_2D);
 
     // add player character sprite
-    auto sprite = Sprite::create("res/character/idle/test_idle_right.png");
-    if (sprite == nullptr)
+    auto character = Sprite::create("res/character/idle/test_idle_right.png");
+    auto mysprite = Sprite::createWithSpriteFrameName("mysprite.png");
+
+    if (character == nullptr)
     {
         problemLoading("'test idle sprite'");
     }
     else
     {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
+        // make pixel art not look blurry
+        character->getTexture()->setAliasTexParameters();
+        // make the character 4 times bigger
+        character->setScale(4.0);
+        // make the center bottom of the character the anchor point
+        character->setAnchorPoint(Vec2(0.5, 0));
+        // position the character on the center of the screen
+        character->setPosition(Vec2(visibleSize.width/2 + origin.x, origin.y + 20));
+        // add the character as a child to this layer
+        this->addChild(character, 0);
     }
+
+    auto direction = "right";
+
+    // create touch listener
+    auto listener1 = EventListenerTouchOneByOne::create();
+    // trigger when you push down
+    listener1->onTouchBegan = [visibleSize, character, direction](Touch* touch, Event* event) mutable {
+        
+        if (touch->getLocation().x > visibleSize.width/2) {
+            character->setTexture("res/character/run/test_right.png");
+            auto action = RepeatForever::create(MoveBy::create(1, Vec2(200, 0)));
+            character->runAction(action);
+            direction = "right";
+        }
+        else {
+            character->setTexture("res/character/run/test_left.png");
+            auto action = RepeatForever::create(MoveBy::create(1, Vec2(-200, 0)));
+            character->runAction(action);
+            direction = "left";
+        }
+        return true; // if you are consuming it
+    };
+
+    // trigger when you let up
+    listener1->onTouchEnded = [character, direction](Touch* touch, Event* event){
+        if (strncmp(direction, "left", 4)) {
+            character->setTexture("res/character/idle/test_idle_left.png");
+        }
+        else if (strncmp(direction, "right", 5)) {
+            character->setTexture("res/character/idle/test_idle_right.png");
+        }
+        else {
+            character->setTexture("res/character/idle/test_idle_right.png");
+        }
+
+        character->stopAllActions();
+    };
+
+    // Add listener
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, character);
+
     return true;
 }
